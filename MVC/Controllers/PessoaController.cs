cbc35f9d92842443ba583df73477f6cf3f52d538
aluno <aluno@LAB>
2023-05-04 20:27:16 -0300

    Adicionar arquivos de projeto.

---
+++ MVC/Controllers/PessoaController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class PessoaController : Controller
+    {
+        public IActionResult Index()
+        {
+            var pessoa = new PessoaModelView();
+
+            pessoa.Name = "Você";
+            pessoa.LastName = "Não viu";
+            pessoa.EndName = "Nada";
+
+            return View(pessoa);
+        }
+
+        [HttpPost] //Puxa o Required de "PessoaModelView"
+        public IActionResult Save(PessoaModelView pessoa)
+        {
+            if (ModelState.IsValid)
+            {
+                //Aqui é aplicada a condição caso seja válida a ModelState(onde estão as nossas variáveis implementadas na view)
+            }
+        }
+    }
+}
---
+++ MVC/Models/PessoaModelView.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC.Models
+{
+    public class PessoaModelView
+    {
+        [Required] //É uma requisição, caso contrário, haverá uma exceção.
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public string EndName { get; set; }
+    }
+}
